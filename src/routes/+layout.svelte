26a7968d78218ae533a280fb234e3ad46aba0a6a
Joel Sequeira <joelseq96@gmail.com>
2023-07-15 16:45:15 -0400

    Fix tailwind preflight issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import '@skeletonlabs/skeleton/themes/theme-crimson.css';
   // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
   import '@skeletonlabs/skeleton/styles/skeleton.css';
+  import '../preflight.css';
   // Most of your app wide CSS should be put in this file
   import '../app.postcss';
   import { AppShell, AppBar, Modal, Toast } from '@skeletonlabs/skeleton';
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,18 @@
-import { join } from 'path'
-import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
+import { join } from 'path';
+import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [...skeleton()],
-}
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [...skeleton()],
+};
